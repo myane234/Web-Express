083f5a4e6834ecd34a6448a5a93d1b4e14db613c
myane234 <flytothemoonkawaii@gmail.com>
2026-01-03 15:22:10 +0700

    add login-register docs

--- src/Handlers/usershandlers.ts
+++ src/Handlers/usershandlers.ts
@@ -1,11 +1,11 @@
 import type{ Request, Response } from "express";
-import bcrypt from 'bcrypt'
+import bcrypt, { compare } from 'bcrypt'
 import { db } from "../database/db.js";
 
 export async function register(req: Request, res: Response) {
     const { nama, password } = req.body;
     try {
-        const [checkUsers]:any[] = await db.query(`SELECT nama, password FROM users LIMIT 5`)
+        const [checkUsers]:any[] = await db.query(`SELECT nama, password FROM users WHERE nama = ?`, [nama])
 
         if(checkUsers.length > 0) {
             return res.status(409).json({
@@ -29,9 +29,76 @@ export async function register(req: Request, res: Response) {
             sukses: true
         })
     } catch(err) {
+        console.error(err)
         return res.status(500).json({
             sukses: false,
             message: 'Server Error'
+        })
+    }
+}
+
+export async function login(req: Request, res: Response) {
+    const { nama, password } = req.body;
+    try {
+
+
+        if(!nama && !password) {
+            return res.status(401).json({
+                message: 'Nama atau Password harus di isi',
+                sukses: false
+            })
+        }
+
+        const [checkUser]:any[] = await db.query(`SELECT nama, password FROM users`, [nama])
+        if(checkUser.length <= 0) {
+            return res.status(400).json({
+                message: 'Nama salah',
+                sukses: false
+            })
+        }
+
+        const comparePw = await compare(password, checkUser[0].password)
+
+        if(!comparePw) {
+            return res.status(401).json({
+                message: 'Password salah',
+                sukses: false
+            })
+        }
+
+        return res.status(200).json({
+            message: 'Ditemukan',
+            sukses: true
+        })
+    } catch(err) {
+        console.error(err)
+        return res.status(500).json({
+            message: 'Server Error',
+            sukses: false
+        })
+    }
+}
+
+export async function allUser(req: Request, res: Response) {
+    try {
+        const [Users]: any[] = await db.query(`SELECT id_user, nama, password FROM users LIMIT 50`)
+
+        if(Users.length < 0) {
+            return res.status(400).json({
+                message: 'User tidak di temukan',
+                sukses: false
+            })
+        }
+
+        return res.status(200).json({
+            User: Users,
+            sukses: true
+        })
+    } catch(err) {
+        console.error(err)
+        return res.status(500).json({
+            message: 'Server Error',
+            sukses: false
         })
     }
 }
--- src/Route/usersRoute.ts
+++ src/Route/usersRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request, Response } from 'express';
-import { Me, register } from "../Handlers/usershandlers.js";
+import { allUser, login, Me, register } from "../Handlers/usershandlers.js";
 
 const route = Router();
 /**
@@ -49,9 +49,141 @@ const route = Router();
 route.get('/me', Me)
 
 /**
- * 
+ * @openapi
+ * /user/register:
+ *   post:
+ *     summary: register new User
+ *     tags:
+ *       - user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nama
+ *               - password
+ *             properties:
+ *               nama:
+ *                 type: string
+ *                 example: faruq
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "123"
+ *     responses:
+ *       200:
+ *         description: Registration successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "User registered successfully"
+ *       400:
+ *         description: Bad request
  */
 route.post('/register', register)
+
+
+/**
+ * @openapi
+ * /user/allUsers:
+ *   get:
+ *     summary: Get all users
+ *     tags:
+ *       - user
+ *     responses:
+ *       200:
+ *         description: User ditemukan
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sukses:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       nama:
+ *                         type: string
+ *                         example: "Faruq"
+ *                       password:
+ *                         type: password
+ *                         example: 1234
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sukses:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Terjadi kesalahan"
+ */
+route.get('/allUsers', allUser)
+
+/**
+ * @openapi
+ * /user/login:
+ *   post:
+ *     summary: login User
+ *     tags:
+ *       - user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nama
+ *               - password
+ *             properties:
+ *               nama:
+ *                 type: string
+ *                 example: faruq
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "123"
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "User Login succes"
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: wrong password or nama
+ */
+route.post('/login', login)
 
 
 export default route;
